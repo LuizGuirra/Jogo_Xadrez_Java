01460cf9cbbc8cfa5e0f3904f831bdb66aa2cfaf
Luiz Eduardo <luizedg.rios1901@gmail.com>
2024-01-17 11:52:44 -0300

    Primeira Classe: Position

--- src/Application/Programa.java
+++ src/Application/Programa.java
@@ -1,10 +1,12 @@
 package Application;
 
+import Tabuleiro.Position;
+
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		
-		
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 	}
 }
