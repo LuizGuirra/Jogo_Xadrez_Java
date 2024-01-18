79403ba2376bd53dc4fb4f70ee1704736463af88
Luiz Eduardo <luizedg.rios1901@gmail.com>
2024-01-18 11:01:45 -0300

    Começando a implementar Tabuleiro e Peça

--- src/Application/Programa.java
+++ src/Application/Programa.java
@@ -1,12 +1,11 @@
 package Application;
 
-import Tabuleiro.Position;
+import Tabuleiro.Board;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		Position pos = new Position(3, 5);
-		System.out.println(pos);
+		Board board = new Board(8, 8);
 	}
 }
